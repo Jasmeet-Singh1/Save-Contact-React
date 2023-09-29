d7aacf42998a1899fcbea0a3b13512234456387e
Jasmeet Singh <jassoffline586@gmail.com>
2023-09-29 14:44:29 +0530

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json({ extended: false }));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+//GET = GETDATA
+//POST = POST OR UPLOAD DATA
+//PUT = UPDATE DATA
+//DELETE= delete data
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to the SaveContact API...' });
+});
+
+//Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
